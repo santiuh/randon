4c00a0e28eaeea54e3808321466a29dc23719082
santiagogirolami <santiagogirolami@gmail.com>
2025-04-14 21:05:58 -0300

    mobile

--- pages/Concesionarios.vue
+++ pages/Concesionarios.vue
@@ -1,95 +1,88 @@
 <template>
-  <div class="w-full bg-gray flex justify-center !text-primary py-16 px-4">
+  <div
+    class="w-full bg-gray flex justify-center !text-primary py-24 lg:py-8 px-4"
+  >
     <div
-      class="shadow rounded-xl bg-[#e0e1e2] px-8 py-4 flex w-full lg:max-w-[1440px]"
+      class="shadow rounded-xl bg-[#e0e1e2] px-4 pt-4 pb-8 lg:pt-4 lg:pb-4 flex flex-col w-full lg:max-w-[1440px]"
     >
-      <table
-        class="min-w-full text-sm text-left text-gray-700 border-separate border-spacing-y-2"
-      >
-        <thead class="bg-gray-100 text-secondary text-gray-700">
-          <tr>
-            <th class="py-2 pl-4">Concesionario</th>
-            <th class="py-2">Localidad</th>
-            <th class="py-2">Provincia</th>
-            <th class="py-2">Dirección</th>
-            <th class="py-2">Teléfonos</th>
-            <th class="py-2">Mail</th>
-            <th class="py-2">Web</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(entry, i) in rawData" :key="i" class="bg-[#cccdce]">
-            <td class="pl-4 font-bold rounded-l-xl">
-              {{ entry.concesionario }}
-            </td>
-            <td class="py-2">
-              <ul>
-                <li
-                  class="py-1"
-                  v-for="(localidad, idx) in entry.localidad"
-                  :key="idx"
+      <div class="overflow-x-auto">
+        <table
+          class="min-w-full text-sm text-left text-gray-700 border-separate border-spacing-y-2"
+        >
+          <thead class="bg-gray-100 text-secondary text-gray-700">
+            <tr>
+              <th class="py-2 pl-2 min-w-[120px]">Concesionario</th>
+              <th class="py-2 min-w-[100px]">Localidad</th>
+              <th class="py-2 min-w-[100px]">Provincia</th>
+              <th class="py-2 min-w-[150px]">Dirección</th>
+              <th class="py-2 min-w-[150px]">Teléfonos</th>
+              <th class="py-2 min-w-[180px]">Mail</th>
+              <th class="py-2 min-w-[120px] rounded-r-xl">Web</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(entry, i) in rawData" :key="i" class="bg-[#cccdce]">
+              <td class="pl-2 font-bold rounded-l-xl">
+                {{ entry.concesionario }}
+              </td>
+              <td class="py-2">
+                <ul class="leading-tight">
+                  <li
+                    class="text-nowrap"
+                    v-for="(localidad, idx) in entry.localidad"
+                    :key="idx"
+                  >
+                    {{ localidad }}
+                  </li>
+                </ul>
+              </td>
+              <td class="py-2">
+                <ul class="leading-tight">
+                  <li v-for="(provincia, idx) in entry.provincia" :key="idx">
+                    {{ provincia }}
+                  </li>
+                </ul>
+              </td>
+              <td class="py-2">
+                <ul class="leading-tight">
+                  <li v-for="(direccion, idx) in entry.direccion" :key="idx">
+                    {{ direccion }}
+                  </li>
+                </ul>
+              </td>
+              <td class="py-2">
+                <ul class="leading-tight">
+                  <li v-for="(tel, idx) in entry.telefonos" :key="idx">
+                    <span v-if="Array.isArray(tel)">{{ tel.join(", ") }}</span>
+                    <span v-else>{{ tel }}</span>
+                  </li>
+                </ul>
+              </td>
+              <td class="py-2">
+                <ul class="leading-tight">
+                  <li
+                    v-for="(email, idx) in normalizeArray(entry.mail)"
+                    :key="idx"
+                  >
+                    <a :href="'mailto:' + email" class="hover:underline">{{
+                      email
+                    }}</a>
+                  </li>
+                </ul>
+              </td>
+              <td class="py-2 rounded-r-xl">
+                <a
+                  class="text-blue-600 hover:underline truncate block"
+                  :href="'https://' + entry.web"
+                  target="_blank"
                 >
-                  {{ localidad }}
-                </li>
-              </ul>
-            </td>
-            <td class="py-2">
-              <ul>
-                <li
-                  class="py-1"
-                  v-for="(provincia, idx) in entry.provincia"
-                  :key="idx"
-                >
-                  {{ provincia }}
-                </li>
-              </ul>
-            </td>
-            <td class="py-2">
-              <ul>
-                <li
-                  class="py-1"
-                  v-for="(direccion, idx) in entry.direccion"
-                  :key="idx"
-                >
-                  {{ direccion }}
-                </li>
-              </ul>
-            </td>
-            <td class="py-2">
-              <ul>
-                <li
-                  class="py-1"
-                  v-for="(tel, idx) in entry.telefonos"
-                  :key="idx"
-                >
-                  <span v-if="Array.isArray(tel)">{{ tel.join(", ") }}</span>
-                  <span v-else>{{ tel }}</span>
-                </li>
-              </ul>
-            </td>
-            <td class="py-2">
-              <ul>
-                <li
-                  class="py-1"
-                  v-for="(email, idx) in normalizeArray(entry.mail)"
-                  :key="idx"
-                >
-                  {{ email }}
-                </li>
-              </ul>
-            </td>
-            <td class="py-2 rounded-r-xl">
-              <a
-                class="text-blue-600 hover:underline"
-                :href="'https://' + entry.web"
-                target="_blank"
-              >
-                {{ entry.web }}
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                  {{ entry.web }}
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
