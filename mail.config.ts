87a01ccd096161d156037b2c94a6df16cae2cb22
santiagogirolami <santiagogirolami@gmail.com>
2025-07-04 11:49:01 -0300

    delete mailcfg

--- mail.config.ts
+++
@@ -1,9 +0,0 @@
-// Configuración de nuxt-mail para SMTP
-export default {
-  mail: {
-    message: {
-      to: "ventas@randon.com.ar", // Cambia esto por el destinatario real
-    },
-    // La configuración SMTP se trasladó a runtimeConfig para la compatibilidad con Nuxt 3
-  },
-};
